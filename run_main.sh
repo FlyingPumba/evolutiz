638d5c7af53ce7ac0199d240dce1a0903eb36e9f
Ivan Arcuschin <iarcuschin@gmail.com>
2017-09-21 18:37:38 -0300

    Added log.err file in run_main.sh

--- run_main.sh
+++ run_main.sh
@@ -6,6 +6,6 @@ echo "Settings used: \n" >> $LOG
 cat settings.py >> $LOG
 echo "\n" >> $LOG
 
-python main.py subjects/a2dp.Vol_93_src >> $LOG 2>&1
+python main.py subjects/a2dp.Vol_93_src >> $LOG 2>$LOG.err
 
 echo "Finishing time: $(date +%Y-%m-%d_%H-%M)" >> $LOG
