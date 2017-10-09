8305b1f4b2cd7d6eebaa803721eb44ab91cab643
Ivan Arcuschin <iarcuschin@gmail.com>
2017-10-09 14:56:58 -0300

    Push apk to devices in parallel

---
+++ devices/prepare_apk_parallel.py
@@ -0,0 +1,79 @@
+import multiprocessing as mp
+import os
+import subprocess
+
+import settings
+from analysers import static_analyser
+
+
+def push_apk_and_string_xml(device, decoded_dir, package_name, apk_path):
+    static_analyser.upload_string_xml(device, decoded_dir, package_name)
+
+    print "### Installing apk:", apk_path
+    os.system("$ANDROID_HOME/platform-tools/adb -s " + device + " shell rm /mnt/sdcard/bugreport.crash")
+    os.system("$ANDROID_HOME/platform-tools/adb -s " + device + " uninstall " + package_name)
+    os.system("$ANDROID_HOME/platform-tools/adb -s " + device + " install " + apk_path)
+
+
+def prepare_apk(devices, instrumented_app_dir):
+    package_name, apk_path = get_package_name(instrumented_app_dir)
+    # for css subjects
+    if instrumented_app_dir.endswith(".apk"):
+        instrumented_app_dir += "_output"
+        os.system("mkdir -p " + instrumented_app_dir)
+    print "### Working on apk:", package_name
+    # static analysis
+    if settings.ENABLE_STRING_SEEDING:
+        output_dir = None
+        if instrumented_app_dir.endswith(".apk_output"):
+            output_dir = instrumented_app_dir
+        else:
+            output_dir = instrumented_app_dir + "/bin"
+        static_analyser.decode_apk(apk_path, output_dir)
+
+    decoded_dir = None
+    if instrumented_app_dir.endswith(".apk_output"):
+        decoded_dir = instrumented_app_dir + "/" + apk_path.split("/")[-1].split(".apk")[0]
+    else:
+        decoded_dir = instrumented_app_dir + "/bin/" + apk_path.split("/")[-1].split(".apk")[0]
+
+    pool = mp.Pool(processes=len(devices))
+
+    for device in devices:
+        pool.apply_async(push_apk_and_string_xml,
+                         args=(device, decoded_dir, package_name, apk_path))
+
+    pool.close()
+    pool.join()  # intermediate should be in app folder
+
+    os.system("rm -rf " + instrumented_app_dir + "/intermediate")
+    os.system("mkdir -p " + instrumented_app_dir + "/intermediate")
+    os.system("rm -rf " + instrumented_app_dir + "/crashes")
+    os.system("mkdir -p " + instrumented_app_dir + "/crashes")
+    os.system("rm -rf " + instrumented_app_dir + "/coverages")
+    os.system("mkdir -p " + instrumented_app_dir + "/coverages")
+    return package_name
+
+
+    def get_package_name(path):
+        apk_path = None
+        if path.endswith(".apk"):
+            apk_path = path
+        else:
+            # build the apk
+            # settings.PROJECT_FOLDER = path
+            # any_device.pack_and_deploy_aut()
+
+            # now find its name
+            for file_name in os.listdir(path + "/bin"):
+                if file_name == "bugroid-instrumented.apk":
+                    apk_path = path + "/bin/bugroid-instrumented.apk"
+                    break
+                elif file_name.endswith("-debug.apk"):
+                    apk_path = path + "/bin/" + file_name
+
+        assert apk_path is not None
+
+        get_package_cmd = "$ANDROID_HOME/build-tools/26.0.1/aapt d xmltree " + apk_path + " AndroidManifest.xml | grep package= | awk 'BEGIN {FS=\"\\\"\"}{print $2}'"
+        package_name = subprocess.Popen(get_package_cmd, shell=True, stdout=subprocess.PIPE).communicate()[0].strip()
+        return package_name, apk_path
--- main.py
+++ main.py
@@ -29,9 +29,7 @@
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 
-import os
 import platform
-import subprocess
 import sys
 
 from deap import tools, base
@@ -45,8 +43,8 @@ from algorithms.eaOnePlusLambdaCommaLambdaParallel import eaOnePlusLambdaCommaLa
 from algorithms.eaStandardParallel import eaStandardParallel
 from algorithms.eaSteadyStateParallel import eaSteadyStateParallel
 from algorithms.gen_individual import gen_individual
-from analysers import static_analyser
 from devices import any_device
+from devices.prepare_apk_parallel import prepare_apk
 from init import initRepeatParallel
 
 
@@ -95,67 +93,6 @@ def main(instrumented_app_dir, eaStrategy):
 	for ind in population:
 		print "Individual with fitness: ", ind.fitness
 
-
-def prepare_apk(devices, instrumented_app_dir):
-	package_name, apk_path = get_package_name(instrumented_app_dir)
-	# for css subjects
-	if instrumented_app_dir.endswith(".apk"):
-		instrumented_app_dir += "_output"
-		os.system("mkdir -p " + instrumented_app_dir)
-	print "### Working on apk:", package_name
-	# static analysis
-	if settings.ENABLE_STRING_SEEDING:
-		output_dir = None
-		if instrumented_app_dir.endswith(".apk_output"):
-			output_dir = instrumented_app_dir
-		else:
-			output_dir = instrumented_app_dir + "/bin"
-		static_analyser.decode_apk(apk_path, output_dir)
-	# will use dummy 0 if disabled
-	for device in devices:
-		decoded_dir = None
-		if instrumented_app_dir.endswith(".apk_output"):
-			decoded_dir = instrumented_app_dir + "/" + apk_path.split("/")[-1].split(".apk")[0]
-		else:
-			decoded_dir = instrumented_app_dir + "/bin/" + apk_path.split("/")[-1].split(".apk")[0]
-		static_analyser.upload_string_xml(device, decoded_dir, package_name)
-
-		print "### Installing apk:", apk_path
-		os.system("$ANDROID_HOME/platform-tools/adb -s " + device + " shell rm /mnt/sdcard/bugreport.crash")
-		os.system("$ANDROID_HOME/platform-tools/adb -s " + device + " uninstall " + package_name)
-		os.system("$ANDROID_HOME/platform-tools/adb -s " + device + " install " + apk_path)
-
-	# intermediate should be in app folder
-	os.system("rm -rf " + instrumented_app_dir + "/intermediate")
-	os.system("mkdir -p " + instrumented_app_dir + "/intermediate")
-	os.system("rm -rf " + instrumented_app_dir + "/crashes")
-	os.system("mkdir -p " + instrumented_app_dir + "/crashes")
-	os.system("rm -rf " + instrumented_app_dir + "/coverages")
-	os.system("mkdir -p " + instrumented_app_dir + "/coverages")
-	return package_name
-
-def get_package_name(path):
-	apk_path = None
-	if path.endswith(".apk"):
-		apk_path = path
-	else:
-		# build the apk
-		# settings.PROJECT_FOLDER = path
-		# any_device.pack_and_deploy_aut()
-
-		# now find its name
-		for file_name in os.listdir(path + "/bin"):
-			if file_name == "bugroid-instrumented.apk":
-				apk_path = path + "/bin/bugroid-instrumented.apk"
-				break
-			elif file_name.endswith("-debug.apk"):
-				apk_path = path + "/bin/" + file_name
-
-	assert apk_path is not None
-
-	get_package_cmd = "$ANDROID_HOME/build-tools/26.0.1/aapt d xmltree " + apk_path + " AndroidManifest.xml | grep package= | awk 'BEGIN {FS=\"\\\"\"}{print $2}'"
-	package_name = subprocess.Popen(get_package_cmd, shell=True, stdout=subprocess.PIPE).communicate()[0].strip()
-	return package_name, apk_path
 
 if __name__ == "__main__":
 	app_dir = sys.argv[1]
