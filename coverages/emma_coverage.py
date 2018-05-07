f121685e5377a225116c3c2b030a1d5741ce3f61
Ivan <iarcuschin@gmail.com>
2018-05-07 13:51:15 -0300

    Log evaluation results per device

--- coverages/emma_coverage.py
+++ coverages/emma_coverage.py
@@ -84,6 +84,7 @@ def get_suite_coverage(is_motifgene_enabled, scripts, device, result_dir, apk_di
 
 		result_code = adb.push(device, script, "/mnt/sdcard/")
 		if result_code != 0:
+			log_evaluation_result(device, result_dir, script, False)
 			adb.reboot(device)
 			raise Exception("Unable to push motifcore script " + script + " to device: " + adb.get_device_name(device))
 
@@ -97,6 +98,7 @@ def get_suite_coverage(is_motifgene_enabled, scripts, device, result_dir, apk_di
 			# no crash, can broadcast
 			result_code = adb.shell_command(device, "am broadcast -a edu.gatech.m3.emma.COLLECT_COVERAGE")
 			if result_code != 0:
+				log_evaluation_result(device, result_dir, script, False)
 				adb.reboot(device)
 				raise Exception("Unable to broadcast coverage gathering for script " + script + " in device: " + adb.get_device_name(device))
 			there_is_coverage = True
@@ -113,12 +115,14 @@ def get_suite_coverage(is_motifgene_enabled, scripts, device, result_dir, apk_di
 					break
 
 			if not found_coverage_file:
+				log_evaluation_result(device, result_dir, script, False)
 				adb.reboot(device)
 				raise Exception("Coverage broadcast was sent for script " + script + " in device: " + adb.get_device_name(device) + " but there is not file: " + coverage_path_in_device)
 
 			# save coverage.ec file to /mnt/sdcard before clearing app (files are deleted)
 			result_code = adb.sudo_shell_command(device, "cp -p " + coverage_path_in_device + " " + coverage_backup_path_before_clear)
 			if result_code != 0:
+				log_evaluation_result(device, result_dir, script, False)
 				adb.reboot(device)
 				raise Exception("Unable to retrieve coverage.ec file after coverage broadcast for script " + script + " in  device: " + adb.get_device_name(device))
 
@@ -129,8 +133,12 @@ def get_suite_coverage(is_motifgene_enabled, scripts, device, result_dir, apk_di
 		if there_is_coverage:
 			result_code = adb.sudo_shell_command(device, "cp -p " + coverage_backup_path_before_clear + " " + coverage_path_in_device)
 			if result_code != 0:
+				log_evaluation_result(device, result_dir, script, False)
 				adb.reboot(device)
 				raise Exception("Unable to copy backup coverage.ec file in sdcard for script " + script + " in device: " + adb.get_device_name(device))
+
+
+		log_evaluation_result(device, result_dir, script, True)
 
 	print "### Getting EMMA coverage.ec and report ..."
 	adb.shell_command(device, "pm clear " + package_name)
@@ -139,8 +147,9 @@ def get_suite_coverage(is_motifgene_enabled, scripts, device, result_dir, apk_di
 	if there_is_coverage:
 		result_code = adb.pull(device, coverage_backup_path_before_clear, "coverage.ec")
 		if result_code != 0:
+			log_evaluation_result(device, result_dir, "pull-coverage", False)
 			adb.reboot(device)
-			raise Exception("Unable to pull coverage for script " + script + " in device: " + adb.get_device_name(device))
+			raise Exception("Unable to pull coverage for device: " + adb.get_device_name(device))
 
 		os.system("java -cp " + settings.WORKING_DIR + "lib/emma.jar emma report -r html -in coverage.em,coverage.ec -sp " + apk_dir + "/src " + logger.redirect_string())
 
@@ -150,6 +159,7 @@ def get_suite_coverage(is_motifgene_enabled, scripts, device, result_dir, apk_di
 		if coverage_str.find("%") != -1:
 			return int(coverage_str.split("%")[0]), len(unique_crashes)
 		else:
+			log_evaluation_result(device, result_dir, "html-extract-coverage", False)
 			return 0, len(unique_crashes)
 	else:
 		return 0, len(unique_crashes)
@@ -173,3 +183,7 @@ def run_script_using_motifcore(use_motifgene, device, package_name, script_name)
 
 	# need to manually kill motifcore when timeout
 	adb.pkill(device, "motifcore")
+
+def log_evaluation_result(device, result_dir, script, success):
+	device_adb_log_file = result_dir + "/" + adb.get_device_name(device) + "-evaluations.log"
+	os.system("echo \"" + str(success) + " -> " + script + "\" >> " + device_adb_log_file)
