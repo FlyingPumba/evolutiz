e39176100ad2d82e0af529effed30f65f2bbd2d8
Ivan Arcuschin <iarcuschin@gmail.com>
2018-06-07 22:15:30 -0300

    Script to run exp

---
+++ run_exp.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+python run_sapienz.py > tmux-output-08-6-random-2.txt
+python run_sapienz.py -s random > tmux-output-08-6-random-2.txt
+python run_sapienz.py --no-motifgene > tmux-output-08-6-no-motifgene-2.txt
