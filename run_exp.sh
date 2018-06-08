68648091714072262c9b2e26a4c02e336be03928
Iván Arcuschin <iarcuschin@gmail.com>
2018-06-08 10:48:38 -0300

    Minor changes to script run_exp.sh

--- run_exp.sh
+++ run_exp.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-python run_sapienz.py > tmux-output-08-6-random-2.txt
-python run_sapienz.py -s random > tmux-output-08-6-random-2.txt
-python run_sapienz.py --no-motifgene > tmux-output-08-6-no-motifgene-2.txt
+python run_sapienz.py > tmux-output-08-6-random-$1.txt
+python run_sapienz.py -s random > tmux-output-08-6-random-$1.txt
+python run_sapienz.py --no-motifgene > tmux-output-08-6-no-motifgene-$1.txt
+mkdir results-$1
+mv monkey/results/* results-$1/
+mv output* results-$1/
+mv tmux* results-$1/
