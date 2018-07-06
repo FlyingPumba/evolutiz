93a4009420a84dc91e295152ea68a54a735bf940
Ivan <iarcuschin@gmail.com>
2018-07-06 18:35:14 -0300

    Changes to AUC script

--- calculate_efficiency_auc.py
+++ calculate_efficiency_auc.py
@@ -1,10 +1,10 @@
 import sys
 
 delta_time = 5 * 60 # 5 minutes
-if sys.argv.length > 2:
-  delta_time = sys.argv[1]
+if len(sys.argv) > 1:
+  delta_time = float(sys.argv[1])
 
-print "Using Delta time: " + str(delta_time) + "m"
+print "Using Delta time: " + str(delta_time) + "s"
 
 # gather_data
 coverage_data = []
@@ -18,59 +18,79 @@ with open('./fitness-historic.log', 'r') as historic_file:
 
   for line in lines:
       timestamp, coverage, crashes, length = line.split(',')
-      timestamp_data.append(timestamp)
-      coverage_data.append(coverage)
-      crashes_data.append(crashes)
-      length_data.append(length)
+      timestamp_data.append(float(timestamp))
+      coverage_data.append(float(coverage))
+      crashes_data.append(float(crashes))
+      length_data.append(float(length))
 
-# process data
-coverage_sum = 0
-crashes_sum = 0
-length_sum = 0
+# accumulate data
+coverage_accumulated_data = []
+crashes_accumulated_data = []
+length_accumulated_data = []
 
-coverage_best = 0
-crashes_best = 0
+coverage_best = -1
+crashes_best = -1
 length_best = sys.maxint
 
-for index, current_timestamp in enumerate(timestamp_data[:-1]):
-  next_timestamp = timestamp_data[index + 1]
-  delta_time = next_timestamp - current_timestamp
+start_time = timestamp_data[0]
+end_time = timestamp_data[-1]
 
-  # i-th data
-  if coverage_data[index] > coverage_best:
+delta_time_slot_start = start_time
+delta_time_slot_end = delta_time_slot_start + delta_time
+
+index = 0
+while index < len(timestamp_data):
+  next_timestamp = timestamp_data[index]
+
+  if next_timestamp > delta_time_slot_end:
+    #  init new delta time
+    delta_time_slot_start = delta_time_slot_end
+    delta_time_slot_end = delta_time_slot_start + delta_time
+
+    # save best values that occurred in elapsed delta time
+    coverage_accumulated_data.append(coverage_best)
+    crashes_accumulated_data.append(crashes_best)
+    length_accumulated_data.append(length_best)
+    # reset values
+    #coverage_best = -1
+    #crashes_best = -1
+    #length_best = sys.maxint
+
+  # check if we increased a value
+  if coverage_data[index] > coverage_best or coverage_best == -1:
     coverage_best = coverage_data[index]
-  current_coverage = coverage_best
 
-  if crashes_data[index] > crashes_best:
+  if crashes_data[index] > crashes_best or crashes_best == -1:
     crashes_best = crashes_data[index]
-  current_crashes = crashes_best
 
-  if length_data[index] > length_best:
+  if length_data[index] < length_best or length_best == sys.maxint:
     length_best = length_data[index]
-  current_length = length_best
 
-  #i+1-th data
-  if coverage_data[index + 1] > coverage_best:
-    coverage_best = coverage_data[index + 1]
-  next_coverage = coverage_best
+  index += 1
 
-  if crashes_data[index + 1] > crashes_best:
-    crashes_best = crashes_data[index + 1]
-  next_crashes = crashes_best
+if coverage_best != -1:
+  # push best values from last delta time
+  coverage_accumulated_data.append(coverage_best)
+  crashes_accumulated_data.append(crashes_best)
+  length_accumulated_data.append(length_best)
 
-  if length_data[index + 1] > length_best:
-    length_best = length_data[index + 1]
-  next_length = length_best
+# process accumulated data
+total_time = len(coverage_accumulated_data) * delta_time
+coverage_auc = 0
+crashes_auc = 0
+length_auc = 0
 
-  coverage_sum += (current_coverage + next_coverage) * delta_time
-  crashes_sum += (current_crashes + next_crashes) * delta_time
-  length_sum += (current_length + next_length) * delta_time
+index = 0
+while index < len(coverage_accumulated_data) - 1:
+  coverage_auc += (coverage_accumulated_data[index] + coverage_accumulated_data[index + 1])
+  crashes_auc += (crashes_accumulated_data[index] + crashes_accumulated_data[index + 1])
+  length_auc += (length_accumulated_data[index] + length_accumulated_data[index + 1])
 
-# calcualte AUC
-total_time = timestamp_data[-1] - timestamp_data[0]
-coverage_auc = coverage_sum / float(2 * total_time)
-crashes_auc = crashes_sum / float(2 * total_time)
-length_auc = length_sum / float(2 * total_time)
+  index += 1
+
+coverage_auc = (coverage_auc * delta_time) / (2 * total_time)
+crashes_auc = (crashes_auc * delta_time) / (2 * total_time)
+length_auc = (length_auc * delta_time) / (2 * total_time)
 
 print "Coverage AUC: " + str(coverage_auc)
 print "Crashes AUC: " + str(crashes_auc)
