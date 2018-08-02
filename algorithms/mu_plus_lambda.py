c32269a1934388af1a9dacd7b9cc50f39bb5ae31
Ivan <iarcuschin@gmail.com>
2018-08-02 18:51:59 -0300

    One mor fix to mu_plus_lambda

--- algorithms/mu_plus_lambda.py
+++ algorithms/mu_plus_lambda.py
@@ -39,7 +39,7 @@ class MuPlusLambda(object):
         self.stats = stats
         self.verbose = verbose
 
-        self.targets_historic_log_file = self.toolbox.get_result_dir() + "/targets-historic.log"
+        self.targets_historic_log_file = self.result_dir + "/targets-historic.log"
         self.setup_log_best_historic_objectives_achieved()
 
     def run(self):
