007090f8c74ada39947cd4cd0e5f03ae764f2c5d
Ivan <iarcuschin@gmail.com>
2018-08-03 15:04:59 -0300

    Fixed several bugs in parallel_evaluator

--- test_suite_evaluation/parallel_evaluator.py
+++ test_suite_evaluation/parallel_evaluator.py
@@ -10,6 +10,7 @@ class ParallelEvaluator(object):
 
     def __init__(self):
         self.device_manager = RequiredFeature('device_manager').request()
+        self.budget_manager = RequiredFeature('budget_manager').request()
         self.test_suite_evaluator = RequiredFeature('test_suite_evaluator').request()
         self.toolbox = RequiredFeature('toolbox').request()
 
@@ -31,23 +32,6 @@ class ParallelEvaluator(object):
                                 str(len(self.individuals_evaluated)) + "/" + str(self.total_individuals))
             logger.log_fitness_result(fitness)
 
-    def evaluation_wrapper(self, individual, device, gen, pop):
-        try:
-            print "starting eval_suite_parallel_wrapper for individual ", pop
-            start_time = time.time()
-
-            result = self.test_suite_evaluator.evaluate(individual, device, gen, pop)
-
-            elapsed_time = time.time() - start_time
-            print "Elapsed seconds to evaluate individual was ", elapsed_time
-
-            return result
-        except Exception as e:
-            logger.log_progress("\nThere was an error evaluating individual in parallel on device:" +
-                                device.name + "\n")
-            print e
-            return pop, False, device
-
     def evaluate(self, individuals, gen):
         if len(self.individuals_evaluated) > 0:
             self.individuals_evaluated = []
@@ -68,9 +52,9 @@ class ParallelEvaluator(object):
         # 2. aissign tasks to devices
         pool = mp.Pool(processes=len(self.idle_devices))
         time_out = False
-        while len(self.individuals_evaluated) > 0:
+        while len(self.individuals_evaluated) < self.total_individuals:
 
-            if not self.toolbox.time_budget_available():
+            if not self.budget_manager.time_budget_available():
                 print "Time budget run out, exiting parallel evaluation"
                 time_out = True
                 break
@@ -80,7 +64,7 @@ class ParallelEvaluator(object):
                 index = remaining_index_to_evaluate.pop(0)
 
                 pool.apply_async(pickable_function,
-                                 args=(self, 'evaluation_wrapper', (individuals[index], device, gen, index,)),
+                                 args=(self.test_suite_evaluator, 'evaluate', (individuals[index], device, gen, index,)),
                                  callback=self.process_results)
             else:
                 time.sleep(2)
