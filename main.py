b0d28fb5078b3b79acb12e6f256e98f29c970792
Ivan Arcuschin <iarcuschin@gmail.com>
2018-08-06 00:32:38 -0300

    Add capability to parse arguments from config file

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import sys
+
+import ConfigParser
 import argparse
 import os
 import random
+import re
 import subprocess as sub
 import traceback
 
@@ -34,7 +38,6 @@ from util.command_checker import is_command_available
 
 
 def run_one_app(strategy_with_runner_name):
-    device_manager = RequiredFeature('device_manager').request()
     app_path = RequiredFeature('app_path').request()
     repetitions = RequiredFeature('repetitions').request()
     budget_manager = RequiredFeature('budget_manager').request()
@@ -44,35 +47,11 @@ def run_one_app(strategy_with_runner_name):
     try:
         for repetition in range(0, repetitions):
 
-            # prepare result_dir folder
-            result_dir = settings.WORKING_DIR + "results/" + \
-                         strategy_with_runner_name + "/" + app_name + "/" + str(repetition)
+            result_dir = prepare_result_dir(app_name, repetition, strategy_with_runner_name)
 
-            os.system("rm -rf " + result_dir + "/*" + logger.redirect_string())
-
-            result_code = os.system("mkdir -p " + result_dir)
-            if result_code != 0: raise Exception("Unable to create result dir")
-            os.system("mkdir -p " + result_dir + "/intermediate")
-            os.system("mkdir -p " + result_dir + "/coverage")
-            os.system("mkdir -p " + result_dir + "/crashes")
-
-            features.provide('result_dir', result_dir)
-            adb.adb_logs_dir = result_dir
-
-            #  reboot all devices and restart adb server before starting a repetition
-            adb.restart_server()
-            if len(device_manager.get_devices(refresh=True)) < device_manager.get_total_number_of_devices_expected():
-                device_manager.boot_emulators(wait_to_be_ready=True)
-            else:
-                device_manager.reboot_devices(wait_to_be_ready=True)
+            prepare_devices(result_dir)
 
             logger.log_progress("\n-----> Starting repetition: " + str(repetition) + " for app: " + app_name)
-
-            for device in device_manager.get_devices():
-                device.clean_sdcard()
-
-            device_manager.wait_for_battery_threshold()
-            device_manager.log_devices_battery("init", result_dir)
 
             test_generator = Evolutiz()
 
@@ -86,6 +65,41 @@ def run_one_app(strategy_with_runner_name):
         logger.log_progress("\nThere was an error running evolutiz on app: " + app_name)
         traceback.print_exc()
         return False
+
+
+def prepare_devices(result_dir):
+    """Reboot all devices and restart adb server before starting a repetition."""
+    device_manager = RequiredFeature('device_manager').request()
+    adb.restart_server()
+
+    if len(device_manager.get_devices(refresh=True)) < device_manager.get_total_number_of_devices_expected():
+        device_manager.boot_emulators(wait_to_be_ready=True)
+    else:
+        device_manager.reboot_devices(wait_to_be_ready=True)
+
+    for device in device_manager.get_devices():
+        device.clean_sdcard()
+
+    device_manager.wait_for_battery_threshold()
+    device_manager.log_devices_battery("init", result_dir)
+
+
+def prepare_result_dir(app_name, repetition, strategy_with_runner_name):
+    result_dir = settings.WORKING_DIR + "results/" + \
+                 strategy_with_runner_name + "/" + app_name + "/" + str(repetition)
+
+    os.system("rm -rf " + result_dir + "/*" + logger.redirect_string())
+    result_code = os.system("mkdir -p " + result_dir)
+    if result_code != 0: raise Exception("Unable to create result dir")
+
+    os.system("mkdir -p " + result_dir + "/intermediate")
+    os.system("mkdir -p " + result_dir + "/coverage")
+    os.system("mkdir -p " + result_dir + "/crashes")
+
+    features.provide('result_dir', result_dir)
+    adb.adb_logs_dir = result_dir
+
+    return result_dir
 
 
 def run(strategy_name, app_paths):
@@ -152,45 +166,42 @@ def check_needed_commands_available():
     #     raise Exception(cause)
 
 
-if __name__ == "__main__":
-    # parse args
-    parser = argparse.ArgumentParser(description='Run Evolutiz with different strategies.')
+def add_arguments_to_parser(parser):
+    global possible_strategies, possible_test_suite_evaluators, possible_individual_generators, possible_test_runners
 
     # subjects related arguments
     parser.add_argument('-d', '--subjects', dest='subjects_directory',
-                        default='subjects/are-we-there-yet/',
                         help='Directory where subjects are located')
     parser.add_argument('--instrumented-subjects', dest='instrumented_subjects_directory',
-                        default='instrumented-subjects/',
                         help='Directory where instrumented subjects will be located')
     parser.add_argument('--randomize-subjects', dest='randomize_subjects',
-                        action='store_true', default=False, help='Randomize subjects to be processed.')
-    parser.add_argument('--limit-subjects-number', type=int, default=1, dest='limit_subjects_numbers',
+                        action='store_true', help='Randomize subjects to be processed.')
+    parser.add_argument('--limit-subjects-number', type=int, dest='limit_subjects_numbers',
                         help='Limit the number of subjects to be processed (-1 to disable limit).')
 
     # budget related arguments
-    parser.add_argument('-r', '--repetitions', type=int, default=1, dest='repetitions',
+    parser.add_argument('-r', '--repetitions', type=int, dest='repetitions',
                         help='Choose the number of repetitions per subject to run. Default is 1.')
-    parser.add_argument('--time-budget', type=int, default=15*60, dest='time_budget',
+    parser.add_argument('--time-budget', type=int, dest='time_budget',
                         help='Choose the time budget (in seconds). Default is 15 minutes.')
-    parser.add_argument('--evaluations-budget', type=int, default=None, dest='evaluations_budget',
+    parser.add_argument('--evaluations-budget', type=int, dest='evaluations_budget',
                         help='Choose the evaluations budget. Default is unlimited.')
 
     # devices related arguments
-    parser.add_argument('--real-devices-number', type=int, default=0, dest='real_devices_number',
+    parser.add_argument('--real-devices-number', type=int, dest='real_devices_number',
                         help='Set the number of real devices to use. Default is 0.')
-    parser.add_argument('--emulators-number', type=int, default=2, dest='emulators_number',
+    parser.add_argument('--emulators-number', type=int, dest='emulators_number',
                         help='Set the number of emulators to use. Default is 2.')
-    parser.add_argument('--avd-series', type=str, default='Nexus_4_API_19', dest='avd_series',
+    parser.add_argument('--avd-series', type=str, dest='avd_series',
                         help='Set the prefix of avd names. Default is Nexus_4_API_19.')
 
     # logging related arguments
     parser.add_argument('--write-logbook', dest='write_logbook',
-                        action='store_false', default=True, help='Write logbook to a file.')
+                        action='store_false', help='Write logbook to a file.')
     parser.add_argument('--write-history', dest='write_history',
-                        action='store_false', default=True, help='Write genealogical history of individuals to a file.')
+                        action='store_false', help='Write genealogical history of individuals to a file.')
     parser.add_argument('--write-hall-of-fame', dest='write_hall_of_fame',
-                        action='store_false', default=True, help='Write hall of fame of best individuals to a file.')
+                        action='store_false', help='Write hall of fame of best individuals to a file.')
 
     # strategy related arguments
     possible_strategies = {
@@ -203,8 +214,7 @@ if __name__ == "__main__":
         "dynaMosa": DynaMosa,
         "random": Random
     }
-
-    parser.add_argument('-s', '--strategy', dest='selected_strategy', default='muPlusLambda',
+    parser.add_argument('-s', '--strategy', dest='selected_strategy',
                         choices=possible_strategies.keys(), help='Strategy to be used')
 
     # evaluators related argurments
@@ -212,8 +222,7 @@ if __name__ == "__main__":
         "multi-objective": MultiObjectiveTestSuiteEvaluator,
         "single-objective": SingleObjectiveTestSuiteEvaluator
     }
-
-    parser.add_argument('-e', '--evaluator', dest='selected_evaluator', default='multi-objective',
+    parser.add_argument('-e', '--evaluator', dest='selected_evaluator',
                         choices=possible_test_suite_evaluators.keys(), help='Test suite evaluator to be used')
 
     # individual generator related arguments
@@ -221,8 +230,7 @@ if __name__ == "__main__":
         "default": IndividualWithoutCoverageGenerator,
         "with-coverage": IndividualWithCoverageGenerator
     }
-
-    parser.add_argument('-ig', '--individual-generator', dest='selected_individual_generator', default='default',
+    parser.add_argument('-ig', '--individual-generator', dest='selected_individual_generator',
                         choices=possible_individual_generators.keys(), help='Individual generator to be used')
 
     # test runner related arguments
@@ -231,55 +239,138 @@ if __name__ == "__main__":
         "motifcore-nm": MotifcoreTestRunner(use_motifgene=True),
         "evolutiz": EvolutizTestRunner()
     }
-
-    parser.add_argument('-t', '--test-runner', dest='selected_test_runner', default='motifcore',
+    parser.add_argument('-t', '--test-runner', dest='selected_test_runner',
                         choices=possible_test_runners.keys(), help='Test runner to be used')
 
-    # --------------------------------------------------------------------------------- #
-    # --------------------------------------------------------------------------------- #
 
-    # Parse arguments
-    logger.prepare()
-    args = parser.parse_args()
+def init_arguments_defaults():
+    global defaults
+    defaults = {
+        "subjects": "subjects/are-we-there-yet/",
+        "instrumented_subjects": "instrumented-subjects/",
+        "randomize_subjects": False,
+        "limit_subjects_number": 1,
+        "repetitions": 1,
+        "time_budget": 15 * 60,  # 15m
+        "evaluations_budget": None,
+        "real_devices_number": 0,
+        "emulators_number": 2,
+        "avd_series": "Nexus_4_API_19",
+        "write_logbook": True,
+        "write_history": True,
+        "write_hall_of_fame": True,
+        "strategy": "muPlusLambda",
+        "evaluator": "multi-objective",
+        "individual_generator": "default",
+        "test_runner": "motifcore"
+    }
 
+
+def config_items_type_convert(items):
+    result = []
+    for (key, value) in items:
+        try:
+            # remove duplicated whitespaces
+            key = re.sub(' +', ' ', key)
+            type_tag, name = key.split(' ')
+            # convert value to declared type
+            if value == "None":
+                result.append((name, None))
+            elif type_tag == "string":
+                result.append((name, value))
+            elif type_tag == "int":
+                result.append((name, int(value)))
+            elif type_tag == "bool":
+                result.append((name, bool(value)))
+            else:
+                raise ValueError('Invalid type tag "%s" found in config file file.' % type_tag)
+                # alternatively: "everything else defaults to string"
+        except Exception as e:
+            raise ValueError('Unable to convert value for "%s" to declared type "%s".' % (name, type_tag))
+    return result
+
+
+def parse_config_file():
+    global conf_parser, args, remaining_argv
+    # Parse any conf_file specification
+    # We make this parser with add_help=False so that
+    # it doesn't parse -h and print help.
+    conf_parser = argparse.ArgumentParser(
+        # description='Run Evolutiz with different strategies.'
+        description=__doc__,  # printed with -h/--help
+        # Don't mess with format of description
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        # Turn off help, so we print all options in response to -h
+        add_help=False
+    )
+    conf_parser.add_argument("-c", "--conf_file",
+                             help="Specify config file", metavar="FILE")
+    args, remaining_argv = conf_parser.parse_known_args()
+    init_arguments_defaults()
+    if args.conf_file:
+        config = ConfigParser.SafeConfigParser()
+        config.read([args.conf_file])
+        defaults.update(dict(config_items_type_convert(config.items(ConfigParser.DEFAULTSECT))))
+
+
+def provide_features():
     # define subjects
     features.provide('instrumented_subjects_path', args.instrumented_subjects_directory)
-    app_paths = get_subject_paths(args)
-
     # define budget and repetitions
     features.provide('repetitions', args.repetitions)
-    features.provide('budget_manager', BudgetManager(time_budget=args.time_budget, evaluations_budget=args.evaluations_budget))
-
+    features.provide('budget_manager',
+                     BudgetManager(time_budget=args.time_budget, evaluations_budget=args.evaluations_budget))
     # define devices configuration
     features.provide('emulators_number', args.emulators_number)
     features.provide('real_devices_number', args.real_devices_number)
     features.provide('avd_series', args.avd_series)
-
-    # provide features
-    strategy_with_runner_name = args.selected_strategy + "-" + args.selected_test_runner
-
     features.provide('strategy', possible_strategies[args.selected_strategy])
     features.provide('test_suite_evaluator', possible_test_suite_evaluators[args.selected_evaluator])
     features.provide('test_runner', possible_test_runners[args.selected_test_runner])
     features.provide('individual_generator', possible_individual_generators[args.selected_individual_generator])
     features.provide('population_generator', PopulationGenerator)
-
     features.provide('write_logbook', args.write_logbook)
     features.provide('write_history', args.write_history)
     features.provide('write_hall_of_fame', args.write_hall_of_fame)
-
-    check_needed_commands_available()
-
     # singletons
     features.provide('toolbox', Toolbox())
     features.provide('device_manager', DeviceManager())
 
-    # run Evolutiz
+
+if __name__ == "__main__":
+    logger.prepare()
     logger.clear_progress()
+
+    parse_config_file()
+
+    # Parse rest of arguments
+    # Don't suppress add_help here so it will handle -h
+    parser = argparse.ArgumentParser(
+        # Inherit options from config_parser
+        parents=[conf_parser],
+        )
+    parser.set_defaults(**defaults)
+
+    add_arguments_to_parser(parser)
+
+    args = parser.parse_args(remaining_argv)
+
+    # --------------------------------------------------------------------------------- #
+    # --------------------------------------------------------------------------------- #
+
+    check_needed_commands_available()
+
+    app_paths = get_subject_paths(args)
+    strategy_with_runner_name = args.selected_strategy + "-" + args.selected_test_runner
+
+    provide_features()
+
+    # run Evolutiz
     logger.log_progress("Evolutiz (" +
                         args.selected_strategy + ", " +
                         args.selected_evaluator + ", " +
                         args.selected_test_runner + ")")
+
     logger.log_progress("\nSubjects to be processed: " + ''.join(map(lambda p: "\n -" + p, app_paths)))
 
     run(strategy_with_runner_name, app_paths)
