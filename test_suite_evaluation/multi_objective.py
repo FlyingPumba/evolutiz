aaa9d86daf351b204bfd378b66bd588c8457536a
Ivan <iarcuschin@gmail.com>
2018-09-06 18:36:35 -0300

    Fix in multi_objective.py

--- test_suite_evaluation/multi_objective.py
+++ test_suite_evaluation/multi_objective.py
@@ -37,7 +37,7 @@ class MultiObjectiveTestSuiteEvaluator(TestSuiteEvaluator):
                                                                                                individual.generation,
                                                                                                individual.index_in_generation)
         # remove from suite lengths the scripts that did NOT cause a crash
-        for script, had_crash in scripts_crash_status.iteritems():
+        for script, had_crash in scripts_crash_status.items():
             if not had_crash:
                 suite_lengths.pop(script, None)
 
