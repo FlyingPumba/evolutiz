8e96a8b6bb62abccf122bd8622f10797e52208d7
Ivan <iarcuschin@gmail.com>
2018-09-07 19:52:56 -0300

    Added debuggin in command.py

--- util/command.py
+++ util/command.py
@@ -2,6 +2,8 @@ import os
 import subprocess
 
 import settings
+from dependency_injection.required_feature import RequiredFeature
+from util import logger
 
 
 def is_command_available(command):
@@ -10,6 +12,10 @@ def is_command_available(command):
     return result_code == 0
 
 def run_cmd(command):
+    verbose_level = RequiredFeature('verbose_level').request()
+    if verbose_level > 1:
+        logger.log_progress("Running command: %s" % command)
+
     p = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True,
                        timeout=settings.ADB_REGULAR_COMMAND_TIMEOUT, encoding="utf-8")
     return p.stdout, p.stderr
