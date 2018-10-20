a44db723eeebbb6fc0de709462aed67c4919d4be
Ivan Arcuschin <iarcuschin@gmail.com>
2018-10-20 19:23:06 -0300

    Performance improvement in crash_handler

--- crashes/crash_handler.py
+++ crashes/crash_handler.py
@@ -16,53 +16,42 @@ def handle(device, result_dir, script_path, gen, pop, index, unique_crashes):
     :return: True if it is a real crash
     """
 
-    bugreport_suffix = ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(10))
-    local_bugreport_path = result_dir + "/bugreport." + bugreport_suffix + ".crash"
-
     device_bugreport_path = "/mnt/sdcard/bugreport.crash"
 
-    if not adb.exists_file(device, device_bugreport_path):
-        # no crash
+    individual_suffix = str(gen) + "." + str(pop) + "." + str(index)
+    local_bugreport_path = result_dir + "/crashes/bugreport." + individual_suffix
+
+    # save the crash report
+    output, errors, result_code = adb.pull(device, device_bugreport_path, local_bugreport_path)
+    if result_code != 0:
         return False
-    else:
-        # save the crash report
-        output, errors, result_code = adb.pull(device, device_bugreport_path, local_bugreport_path)
-        if result_code != 0:
-            device.flag_as_malfunctioning()
-            raise Exception("Failed to retrieve bugreport.crash file from device: " + device.name)
 
-        # filter duplicate crashes
-        with open(local_bugreport_path) as bug_report_file:
-            content = ""
-            for line_no, line in enumerate(bug_report_file):
-                if line_no == 0:
-                    # should not caused by android itself
-                    if line.startswith("// CRASH: com.android."):
-                        adb.shell_command(device, "rm " + device_bugreport_path)
-                        os.system("rm " + local_bugreport_path)
+    # get content
+    with open(local_bugreport_path) as bug_report_file:
+        content = bug_report_file.read()
 
-                        # caught a crash, but it was Android related
-                        return True
+    # delete remote file
+    adb.shell_command(device, "rm " + device_bugreport_path)
 
-                    continue
-                content += line
-            if content in unique_crashes:
-                adb.shell_command(device, "rm " + device_bugreport_path)
-                os.system("rm " + local_bugreport_path)
+    # should not caused by android itself
+    if content[0].startswith("// CRASH: com.android."):
+        os.system("rm " + local_bugreport_path)
 
-                # caught a crash, but it wasn't a new one
-                return True
-            else:
-                unique_crashes.add(content)
-
-        individual_suffix = str(gen) + "." + str(pop) + "." + str(index)
-        os.system("mv " + local_bugreport_path + " "
-                  + result_dir + "/crashes/bugreport." + individual_suffix)
-
-        # save the script, indicate its ith gen
-        os.system("cp " + script_path + " "
-                  + result_dir + "/crashes/" + "script." + individual_suffix)
-
-        print("### Caught a crash.")
-        adb.shell_command(device, "rm " + device_bugreport_path)
+        # caught a crash, but it was Android related
         return True
+
+    # drop first line
+    content = content[1:]
+
+    # filter duplicate crashes
+    if content in unique_crashes:
+        os.system("rm " + local_bugreport_path)
+
+        # caught a crash, but it wasn't a new one
+        return True
+
+    unique_crashes.add(content)
+
+    # save the script, indicate its ith gen
+    os.system("cp " + script_path + " " + result_dir + "/crashes/" + "script." + individual_suffix)
+    return True
