f5519c8c65c7e129f5538ad519175fd20f933b33
Ivan Arcuschin <iarcuschin@gmail.com>
2018-10-25 00:01:33 -0300

    Performance improvement when extracting coverage from emm-generated html file

--- coverage/coverage_extractor.py
+++ coverage/coverage_extractor.py
@@ -1,12 +1,7 @@
-from bs4 import UnicodeDammit
 from lxml import html
 
 
 def extract_coverage(path):
-    with open(path, 'rb') as file:
-        content = file.read()
-        doc = UnicodeDammit(content, is_html=True)
-
-    parser = html.HTMLParser(encoding=doc.original_encoding)
-    root = html.document_fromstring(content, parser=parser)
-    return root.xpath('/html/body/table[2]/tr[2]/td[5]/text()')[0].strip()
+    tree = html.parse(path)
+    coverage = tree.getroot().xpath('/html/body/table[2]/tr[2]/td[5]/text()')[0].strip()
+    return coverage
