fb696bf1715e51df9d3200b492e103c0b2882e27
Ivan Arcuschin <iarcuschin@gmail.com>
2019-01-06 14:20:22 +0000

    Minor change to hall_of_fame postprocess script

--- postprocess/hall_of_fame.py
+++ postprocess/hall_of_fame.py
@@ -3,26 +3,27 @@ import argparse
 
 from deap import creator, base
 
-creator.create("FitnessCovLen", base.Fitness, weights=(10.0, -0.5, 1000.0))
-creator.create("Individual", list, fitness=creator.FitnessCovLen)
+# For multi-objective:
+# creator.create("FitnessCovLen", base.Fitness, weights=(10.0, -0.5, 1000.0))
+# creator.create("Individual", list, fitness=creator.FitnessCovLen)
+
+# For single-objective:
+creator.create("FitnessMax", base.Fitness, weights=(1.0,))
+creator.create("Individual", list, fitness=creator.FitnessMax)
 
 def show_hof(hof_file_path):
-    try:
-        hof_file = open(hof_file_path)
-        hof = pickle.load(hof_file)
-        print("Hall of fame contains: " + str(len(hof)) + " individuals")
-        print("Stored fitness are: ")
+    hof_file = open(hof_file_path, 'rb')
+    hof = pickle.load(hof_file)
+    print("Hall of fame contains: " + str(len(hof)) + " individuals")
+    print("Stored fitness are: ")
 
-        for individual in hof:
-            coverage, length, crashes = individual.fitness.values
-            if crashes == 0:
-                length = "--"
-            else:
-                length = str(length)
-            print("(%d, %s, %d)" % (coverage, length, crashes))
-
-    except Exception as e:
-        print("Unable to process file: " + hof_file_path)
+    for individual in hof:
+        coverage, length, crashes = individual.fitness.values
+        if crashes == 0:
+            length = "--"
+        else:
+            length = str(length)
+        print("(%d, %s, %d)" % (coverage, length, crashes))
 
 
 if __name__ == "__main__":
