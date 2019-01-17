1f892457aba54248887fbe959244754a444377fb
Ivan Arcuschin <iarcuschin@gmail.com>
2019-01-17 17:57:46 -0300

    Minor change to fix_all.sh script

--- subjects/sapienz-study-2/fix_all.sh
+++ subjects/sapienz-study-2/fix_all.sh
@@ -4,7 +4,7 @@ for i in `ls -d */`; do
   echo "Updating project $i"
   cd $i;
   #cp ../a2dp.Vol_93_src/local.properties ./
-  ../../android-sdk-linux/tools/android update project -t android-10 -p . --subprojects
+  ../../../monkey/android-sdk-linux/tools/android update project -t android-10 -p . --subprojects
   cd ../
 done
 
