a88aacacff3432277004bd3814a1554a2979aa73
Ivan Arcuschin <iarcuschin@gmail.com>
2019-09-17 09:30:16 -0300

    Minor fix to fix_all.sh script in are-we-there-yet subjects

--- subjects/are-we-there-yet/fix_all.sh
+++ subjects/are-we-there-yet/fix_all.sh
@@ -4,6 +4,7 @@ for i in `ls -d */`; do
   echo "Updating project $i"
   cd $i;
   #cp ../a2dp.Vol_93_src/local.properties ./
-  android update project -t android-10 -p . --subprojects
+  ../../../monkey/android-sdk-linux/tools/android update project -t android-10 -p . --subprojects
   cd ../
 done
+
