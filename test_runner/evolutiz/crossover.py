3665375cf6538eff6a014d70a807fe4caa8237bd
Ivan <iarcuschin@gmail.com>
2020-01-16 09:00:59 -0300

    Minor fix

--- test_runner/evolutiz/crossover.py
+++ test_runner/evolutiz/crossover.py
@@ -2,6 +2,8 @@
 import random
 from typing import Tuple
 
+from deap import creator
+
 import settings
 from generation.individual import Individual
 from test_runner.evolutiz.evolutiz_connector import EvolutizConnector
@@ -16,7 +18,7 @@ class EvolutizCrossover(object):
         test_cases_pool = ind1[:] + ind2[:]
         random.shuffle(test_cases_pool)
 
-        new_ind1 = test_cases_pool[:settings.SUITE_SIZE]
-        new_ind2 = test_cases_pool[settings.SUITE_SIZE:]
+        new_ind1: Individual = getattr(creator, Individual.get_name())(test_cases_pool[:settings.SUITE_SIZE])
+        new_ind2: Individual = getattr(creator, Individual.get_name())(test_cases_pool[settings.SUITE_SIZE:])
 
         return new_ind1, new_ind2
